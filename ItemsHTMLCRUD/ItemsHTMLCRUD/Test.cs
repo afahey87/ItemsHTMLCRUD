4e995af41123f5803450e0d826082c38cbe3a743
Fahey <adam.fahey@tylertech.com>
2017-10-07 20:30:53 -0400

    Create and save
    It works!

--- ItemsHTMLCRUD/ItemsHTMLCRUD/Test.cs
+++ ItemsHTMLCRUD/ItemsHTMLCRUD/Test.cs
@@ -6,12 +6,13 @@ using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using NUnit.Framework;
+using OpenQA.Selenium.Support.UI;
 
 namespace Alerts_and_Windows
 {
 
     [TestFixture]
-    class WebDriverTestAlertsandWindows
+    class ItemsHTMLTEST
     {
         IWebDriver driver;
         [SetUp]
@@ -19,6 +20,7 @@ namespace Alerts_and_Windows
         {
             driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://webapps3.tylertech.com/int_dev/qa/munis/adam.fahey/AppHost/items/");
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
         }
         [TearDown]
         public void teardown()
@@ -29,7 +31,32 @@ namespace Alerts_and_Windows
         [Test]
         public void checkItemsPageHasLoaded()
         {
+            waitForPageUntillElementIsVissable(By.Id("walkme-proxy-iframe"), 10);
+            Assert.True(driver.Url.Contains("items"));
+           
+        }
+
+        [Test]
+        public void clickTheAddButton()
+        {
+            
+            driver.FindElement(By.XPath("/html/body/div/div[1]/app-ribbon/div/div[2]/ul/li[3]/button[1]/div")).Click();
+            Assert.True(driver.Url.Contains("Add"));
+            waitForPageUntillElementIsVissable(By.Name("$ctrl.itemForm"), 10);
+            driver.FindElement(By.XPath("/html/body/div/div[2]/item-manager/form/app-item-info-bar/div/div[1]/input")).SendKeys(Keys.Return);
+            driver.FindElement(By.Name("itemDescription")).SendKeys("Autom8");
+            driver.FindElement(By.XPath("/html/body/div/div[2]/item-manager/app-footer/div/div/div/button[1]")).Click();
+            driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div[2]/form/div/input")).SendKeys("Autom8");
+            driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div[3]/div/div/button")).Click();
 
         }
+
+
+        public IWebElement waitForPageUntillElementIsVissable(By locator, int maxseconds)
+        {
+            return new WebDriverWait(driver, TimeSpan.FromSeconds(maxseconds))
+                .Until(ExpectedConditions.ElementExists((locator)));
+        }
+
     }
 }
