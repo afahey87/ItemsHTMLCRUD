434a979a7eead7d42256a2a3aed2359c8a1e1915
Fahey <adam.fahey@tylertech.com>
2017-10-08 21:03:14 -0400

    Finished
    Finished version of Items HTML5 CRUD test.

--- ItemsHTMLCRUD/ItemsHTMLCRUD/Test.cs
+++ ItemsHTMLCRUD/ItemsHTMLCRUD/Test.cs
@@ -37,25 +37,46 @@ namespace Alerts_and_Windows
         }
 
         [Test]
-        public void clickTheAddButton()
+        public void ItemsCRUD()
         {
-            
+            //      Create
+            waitForPageUntillElementIsVissable(By.XPath("/html/body/div/div[2]/search-results/div/div[2]/div/div[1]/table/thead/tr[1]/th[3]"), 10);
             driver.FindElement(By.XPath("/html/body/div/div[1]/app-ribbon/div/div[2]/ul/li[3]/button[1]/div")).Click();
             Assert.True(driver.Url.Contains("Add"));
-            waitForPageUntillElementIsVissable(By.Name("$ctrl.itemForm"), 10);
+            waitOnPage(1);
             driver.FindElement(By.XPath("/html/body/div/div[2]/item-manager/form/app-item-info-bar/div/div[1]/input")).SendKeys(Keys.Return);
+            waitOnPage(1);
             driver.FindElement(By.Name("itemDescription")).SendKeys("Autom8");
+            waitOnPage(1);
             driver.FindElement(By.XPath("/html/body/div/div[2]/item-manager/app-footer/div/div/div/button[1]")).Click();
+            waitOnPage(1);
             driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div[2]/form/div/input")).SendKeys("Autom8");
+            waitForPageUntillElementIsVissable(By.XPath("/html/body/div[1]/div/div/div/div[1]/div/div"), 10);
             driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div[3]/div/div/button")).Click();
+            //      Update
+            waitOnPage(1);
+            driver.FindElement(By.Name("ManufacturerId")).SendKeys("Dell");
+            waitOnPage(1);
+            driver.FindElement(By.Name("ManufacturerId")).SendKeys(Keys.Return);
+            driver.FindElement(By.Name("IsTangible")).Click();
+            driver.FindElement(By.XPath("/html/body/div/div[2]/item-manager/app-footer/div/div/div/button[1]")).Click();
+            driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div[3]/div/div/button")).Click();
+            //      Delete
+            waitOnPage(2);
+            driver.FindElement(By.XPath("/html/body/div/div[1]/app-ribbon/div/div[2]/ul/li[3]/button[2]")).Click();
+            driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div[3]/button[1]")).Click();
 
         }
-
 
         public IWebElement waitForPageUntillElementIsVissable(By locator, int maxseconds)
         {
             return new WebDriverWait(driver, TimeSpan.FromSeconds(maxseconds))
                 .Until(ExpectedConditions.ElementExists((locator)));
+        }
+
+        public void waitOnPage(int seconds)
+        {
+            System.Threading.Thread.Sleep(seconds * 1000);
         }
 
     }
