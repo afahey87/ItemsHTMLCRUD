4d671325d35c923d8e875ec386eb318396b35edf
Fahey <adam.fahey@tylertech.com>
2017-10-10 20:09:23 -0400

    Re org code and comments
    Re org code and comments

--- ItemsHTMLCRUD/ItemsHTMLCRUD/Test.cs
+++ ItemsHTMLCRUD/ItemsHTMLCRUD/Test.cs
@@ -28,45 +28,7 @@ namespace Alerts_and_Windows
             driver.Close();
             driver.Quit();
         }
-        [Test]
-        public void checkItemsPageHasLoaded()
-        {
-            waitForPageUntillElementIsVissable(By.Id("walkme-proxy-iframe"), 10);
-            Assert.True(driver.Url.Contains("items"));      // Verify page is loaded
-           
-        }
 
-        [Test]
-        public void ItemsCRUD()
-        {
-            //      Create
-            waitForPageUntillElementIsVissable(By.XPath("/html/body/div/div[2]/search-results/div/div[2]/div/div[1]/table/thead/tr[1]/th[3]"), 10); // Wait for element to load before test starts.
-            driver.FindElement(By.XPath("/html/body/div/div[1]/app-ribbon/div/div[2]/ul/li[3]/button[1]/div")).Click();     // Click Add button
-            Assert.True(driver.Url.Contains("Add"));
-            waitOnPage(1);
-            driver.FindElement(By.XPath("/html/body/div/div[2]/item-manager/form/app-item-info-bar/div/div[1]/input")).SendKeys(Keys.Return);   // Press return key, This auto generates next number for the Item
-            waitOnPage(1);
-            driver.FindElement(By.Name("itemDescription")).SendKeys("Autom8");
-            waitOnPage(1);
-            driver.FindElement(By.XPath("/html/body/div/div[2]/item-manager/app-footer/div/div/div/button[1]")).Click();    // Click save
-            waitOnPage(1);
-            driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div[2]/form/div/input")).SendKeys("Autom8");     // Reason for save
-            waitForPageUntillElementIsVissable(By.XPath("/html/body/div[1]/div/div/div/div[1]/div/div"), 10);
-            driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div[3]/div/div/button")).Click();    // Confirm Save
-            //      Update
-            waitOnPage(1);
-            driver.FindElement(By.Name("ManufacturerId")).SendKeys("Dell"); // Type in Dell
-            waitOnPage(1);
-            driver.FindElement(By.Name("ManufacturerId")).SendKeys(Keys.Return); // Press return key
-            driver.FindElement(By.Name("IsTangible")).Click(); // Click IsTangiable Radio button
-            driver.FindElement(By.XPath("/html/body/div/div[2]/item-manager/app-footer/div/div/div/button[1]")).Click();    // Click Save
-            driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div[3]/div/div/button")).Click();    // Confrim Save
-            //      Delete
-            waitOnPage(2);
-            driver.FindElement(By.XPath("/html/body/div/div[1]/app-ribbon/div/div[2]/ul/li[3]/button[2]")).Click(); //Click Delet button
-            driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div[3]/button[1]")).Click(); //Confirm delete
-
-        }
 
         public IWebElement waitForPageUntillElementIsVissable(By locator, int maxseconds)
         {
@@ -78,6 +40,65 @@ namespace Alerts_and_Windows
         {
             System.Threading.Thread.Sleep(seconds * 1000);
         }
+
+
+        [Test]
+        public void checkItemsPageHasLoaded()
+        {
+            waitForPageUntillElementIsVissable(By.Id("walkme-proxy-iframe"), 10);
+            // Verify page is loaded
+            Assert.True(driver.Url.Contains("items"));      
+           
+        }
+
+        [Test]
+        public void ItemsCRUD()
+        {
+            //   ***   Create   ***
+
+            // Wait for element to load before test starts.
+            waitForPageUntillElementIsVissable(By.XPath("/html/body/div/div[2]/search-results/div/div[2]/div/div[1]/table/thead/tr[1]/th[3]"), 10);
+            // Click Add button
+            driver.FindElement(By.XPath("/html/body/div/div[1]/app-ribbon/div/div[2]/ul/li[3]/button[1]/div")).Click();     
+            Assert.True(driver.Url.Contains("Add"));
+            waitOnPage(1);
+            // Press return key, This auto generates next number for the Item
+            driver.FindElement(By.XPath("/html/body/div/div[2]/item-manager/form/app-item-info-bar/div/div[1]/input")).SendKeys(Keys.Return);   
+            waitOnPage(1);
+            driver.FindElement(By.Name("itemDescription")).SendKeys("Test123");
+            waitOnPage(1);
+            // Click save
+            driver.FindElement(By.XPath("/html/body/div/div[2]/item-manager/app-footer/div/div/div/button[1]")).Click();    
+            waitOnPage(1);
+            // Reason for save
+            driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div[2]/form/div/input")).SendKeys("Test123");    
+            waitForPageUntillElementIsVissable(By.XPath("/html/body/div[1]/div/div/div/div[1]/div/div"), 10);
+            // Confirm Save
+            driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div[3]/div/div/button")).Click();    
+            //   ***   Update   ***
+            waitOnPage(1);
+            // Type in Dell
+            driver.FindElement(By.Name("ManufacturerId")).SendKeys("Dell"); 
+            waitOnPage(1);
+            // Press return key
+            driver.FindElement(By.Name("ManufacturerId")).SendKeys(Keys.Return);
+            // Click IsTangiable Radio button
+            driver.FindElement(By.Name("IsTangible")).Click();
+            // Click Save
+            driver.FindElement(By.XPath("/html/body/div/div[2]/item-manager/app-footer/div/div/div/button[1]")).Click();
+            // Confrim Save
+            driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div[3]/div/div/button")).Click();  
+            
+
+            //   ***   Delete   ***
+            waitOnPage(2);
+            // Click Delete button
+            driver.FindElement(By.XPath("/html/body/div/div[1]/app-ribbon/div/div[2]/ul/li[3]/button[2]")).Click();
+            // Confirm delete
+            driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div[3]/button[1]")).Click(); 
+        }
+
+     
 
     }
 }
