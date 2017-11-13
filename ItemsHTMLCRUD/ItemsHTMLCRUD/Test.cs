630697ea4e41323bfa8bc06cd602f90d17d9ea84
Adam Fahey <afahey87@gmail.com>
2017-11-13 11:20:00 -0500

    Added Status update
    Added Status update from New to Active.
    Updated the description fields to diplay text correctly.

--- ItemsHTMLCRUD/ItemsHTMLCRUD/Test.cs
+++ ItemsHTMLCRUD/ItemsHTMLCRUD/Test.cs
@@ -47,8 +47,8 @@ namespace Alerts_and_Windows
         {
             waitForPageUntillElementIsVissable(By.Id("walkme-proxy-iframe"), 10);
             // Verify page is loaded
-            Assert.True(driver.Url.Contains("items"));      
-           
+            Assert.True(driver.Url.Contains("items"));
+
         }
 
         [Test]
@@ -59,26 +59,26 @@ namespace Alerts_and_Windows
             // Wait for element to load before test starts.
             waitForPageUntillElementIsVissable(By.XPath("/html/body/div/div[2]/search-results/div/div[2]/div/div[1]/table/thead/tr[1]/th[3]"), 10);
             // Click Add button
-            driver.FindElement(By.XPath("/html/body/div/div[1]/app-ribbon/div/div[2]/ul/li[3]/button[1]/div")).Click();     
+            driver.FindElement(By.XPath("/html/body/div/div[1]/app-ribbon/div/div[2]/ul/li[3]/button[1]/div")).Click();
             Assert.True(driver.Url.Contains("Add"));
             waitOnPage(1);
             // Press return key, This auto generates next number for the Item
-            driver.FindElement(By.XPath("/html/body/div/div[2]/item-manager/form/app-item-info-bar/div/div[1]/input")).SendKeys(Keys.Return);   
-            waitOnPage(1);
-            driver.FindElement(By.Name("itemDescription")).SendKeys("Test123");
+            driver.FindElement(By.XPath("/html/body/div/div[2]/item-manager/form/app-item-info-bar/div/div[1]/input")).SendKeys(Keys.Return);
+            waitOnPage(2);
+            driver.FindElement(By.Name("itemDescription")).SendKeys(" Test123");
             waitOnPage(1);
             // Click save
-            driver.FindElement(By.XPath("/html/body/div/div[2]/item-manager/app-footer/div/div/div/button[1]")).Click();    
+            driver.FindElement(By.XPath("/html/body/div/div[2]/item-manager/app-footer/div/div/div/button[1]")).Click();
             waitOnPage(1);
             // Reason for save
-            driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div[2]/form/div/input")).SendKeys("Test123");    
+            driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div[2]/form/div/input")).SendKeys("Test123");
             waitForPageUntillElementIsVissable(By.XPath("/html/body/div[1]/div/div/div/div[1]/div/div"), 10);
             // Confirm Save
-            driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div[3]/div/div/button")).Click();    
+            driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div[3]/div/div/button")).Click();
             //   ***   Update   ***
             waitOnPage(1);
             // Type in Dell
-            driver.FindElement(By.Name("ManufacturerId")).SendKeys("Dell"); 
+            driver.FindElement(By.Name("ManufacturerId")).SendKeys("Dell");
             waitOnPage(1);
             // Press return key
             driver.FindElement(By.Name("ManufacturerId")).SendKeys(Keys.Return);
@@ -87,18 +87,33 @@ namespace Alerts_and_Windows
             // Click Save
             driver.FindElement(By.XPath("/html/body/div/div[2]/item-manager/app-footer/div/div/div/button[1]")).Click();
             // Confrim Save
-            driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div[3]/div/div/button")).Click();  
-            
+            driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div[3]/div/div/button")).Click();
+
+            // *** Update Again***
+            // Wint on page
+            waitOnPage(3);
+            // Click on dropdown for status of Item. 
+            driver.FindElement(By.XPath("//div[@class='container-flex-vertical']//button[.='New ']")).Click();
+            // wait on page 1 sec. 
+            waitOnPage(1);
+            // Click on Active status. 
+            driver.FindElement(By.XPath("//div[@class='container-flex-vertical']//span[.='Active']")).Click();
+            // Click Save
+            driver.FindElement(By.XPath("/html/body/div/div[2]/item-manager/app-footer/div/div/div/button[1]")).Click();
+            // Confrim Save
+            driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div[3]/div/div/button")).Click();
 
             //   ***   Delete   ***
             waitOnPage(2);
             // Click Delete button
             driver.FindElement(By.XPath("/html/body/div/div[1]/app-ribbon/div/div[2]/ul/li[3]/button[2]")).Click();
             // Confirm delete
-            driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div[3]/button[1]")).Click(); 
+            driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div[3]/button[1]")).Click();
+
+
+
+
+
         }
-
-     
-
     }
 }
